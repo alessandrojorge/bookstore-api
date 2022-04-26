5cd2e89dfa0995045fd5cae08d0bdf78d80934ce
Alessandro Jorge R da Silva <alessandro.jorgers@gmail.com>
2022-04-26 14:57:41 -0300

    Listando todos od livros por categoria

--- src/main/java/com/alessandro/bookstore/resources/LivroResource.java
+++ src/main/java/com/alessandro/bookstore/resources/LivroResource.java
@@ -1,13 +1,19 @@
 package com.alessandro.bookstore.resources;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alessandro.bookstore.domain.Livro;
+import com.alessandro.bookstore.dtos.LivroDTO;
 import com.alessandro.bookstore.service.LivroService;
 
 @RestController
@@ -23,6 +29,23 @@ public class LivroResource {
 		Livro livro = livroService.findById(id);
 		
 		return ResponseEntity.ok().body(livro);
+	}
+		
+	@GetMapping()
+	
+	public ResponseEntity<List<LivroDTO>> findbyAll(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat){
+		//localhos:8080/livros?categoria=1
+		//List<Livro> livro = livroService.findBAll(id_cat);
+		
+		List<Livro> list = livroService.findBAll(id_cat);
+		List<LivroDTO> listDto = list.stream().map(livro-> new LivroDTO(livro)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+		
+		//OU
+		
+		/*return  ResponseEntity.ok().body(livroService.findBAll(id_cat)
+				.stream().map(livro-> new LivroDTO(livro)).collect(Collectors.toList()));*/
+				
 	}
 	
 
